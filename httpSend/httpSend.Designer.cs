d0e2cb49a275f94fc5d732e360d158497d35f949
httpsfernando <brandao.prado@gmail.com>
2025-06-30 08:59:51 -0300

    v1.0.0

--- httpSend/httpSend.Designer.cs
+++ httpSend/httpSend.Designer.cs
@@ -32,6 +32,15 @@
             this.salvar1 = new System.Windows.Forms.Button();
             this.enviar1 = new System.Windows.Forms.Button();
             this.respostaHttpBox = new System.Windows.Forms.TextBox();
+            this.endpoint2 = new System.Windows.Forms.TextBox();
+            this.salvar2 = new System.Windows.Forms.Button();
+            this.enviar2 = new System.Windows.Forms.Button();
+            this.endpoint3 = new System.Windows.Forms.TextBox();
+            this.endpoint4 = new System.Windows.Forms.TextBox();
+            this.salvar3 = new System.Windows.Forms.Button();
+            this.salvar4 = new System.Windows.Forms.Button();
+            this.enviar3 = new System.Windows.Forms.Button();
+            this.enviar4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // endpoint1
@@ -63,17 +72,109 @@
             // 
             // respostaHttpBox
             // 
-            this.respostaHttpBox.Location = new System.Drawing.Point(206, 240);
+            this.respostaHttpBox.Location = new System.Drawing.Point(605, 25);
+            this.respostaHttpBox.Multiline = true;
             this.respostaHttpBox.Name = "respostaHttpBox";
             this.respostaHttpBox.ReadOnly = true;
-            this.respostaHttpBox.Size = new System.Drawing.Size(327, 20);
+            this.respostaHttpBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.respostaHttpBox.Size = new System.Drawing.Size(327, 225);
             this.respostaHttpBox.TabIndex = 3;
+            // 
+            // endpoint2
+            // 
+            this.endpoint2.Location = new System.Drawing.Point(12, 88);
+            this.endpoint2.Name = "endpoint2";
+            this.endpoint2.Size = new System.Drawing.Size(410, 20);
+            this.endpoint2.TabIndex = 4;
+            // 
+            // salvar2
+            // 
+            this.salvar2.Location = new System.Drawing.Point(428, 88);
+            this.salvar2.Name = "salvar2";
+            this.salvar2.Size = new System.Drawing.Size(75, 23);
+            this.salvar2.TabIndex = 5;
+            this.salvar2.Text = "SALVAR";
+            this.salvar2.UseVisualStyleBackColor = true;
+            this.salvar2.Click += new System.EventHandler(this.salvar2_Click);
+            // 
+            // enviar2
+            // 
+            this.enviar2.Location = new System.Drawing.Point(509, 88);
+            this.enviar2.Name = "enviar2";
+            this.enviar2.Size = new System.Drawing.Size(75, 23);
+            this.enviar2.TabIndex = 6;
+            this.enviar2.Text = "ENVIAR";
+            this.enviar2.UseVisualStyleBackColor = true;
+            this.enviar2.Click += new System.EventHandler(this.enviar2_Click);
+            // 
+            // endpoint3
+            // 
+            this.endpoint3.Location = new System.Drawing.Point(12, 148);
+            this.endpoint3.Name = "endpoint3";
+            this.endpoint3.Size = new System.Drawing.Size(410, 20);
+            this.endpoint3.TabIndex = 7;
+            // 
+            // endpoint4
+            // 
+            this.endpoint4.Location = new System.Drawing.Point(12, 207);
+            this.endpoint4.Name = "endpoint4";
+            this.endpoint4.Size = new System.Drawing.Size(410, 20);
+            this.endpoint4.TabIndex = 8;
+            // 
+            // salvar3
+            // 
+            this.salvar3.Location = new System.Drawing.Point(428, 148);
+            this.salvar3.Name = "salvar3";
+            this.salvar3.Size = new System.Drawing.Size(75, 23);
+            this.salvar3.TabIndex = 9;
+            this.salvar3.Text = "SALVAR";
+            this.salvar3.UseVisualStyleBackColor = true;
+            this.salvar3.Click += new System.EventHandler(this.salvar3_Click);
+            // 
+            // salvar4
+            // 
+            this.salvar4.Location = new System.Drawing.Point(428, 207);
+            this.salvar4.Name = "salvar4";
+            this.salvar4.Size = new System.Drawing.Size(75, 23);
+            this.salvar4.TabIndex = 10;
+            this.salvar4.Text = "SALVAR";
+            this.salvar4.UseVisualStyleBackColor = true;
+            this.salvar4.Click += new System.EventHandler(this.salvar4_Click);
+            // 
+            // enviar3
+            // 
+            this.enviar3.Location = new System.Drawing.Point(509, 148);
+            this.enviar3.Name = "enviar3";
+            this.enviar3.Size = new System.Drawing.Size(75, 23);
+            this.enviar3.TabIndex = 11;
+            this.enviar3.Text = "ENVIAR";
+            this.enviar3.UseVisualStyleBackColor = true;
+            this.enviar3.Click += new System.EventHandler(this.enviar3_Click);
+            // 
+            // enviar4
+            // 
+            this.enviar4.Location = new System.Drawing.Point(509, 207);
+            this.enviar4.Name = "enviar4";
+            this.enviar4.Size = new System.Drawing.Size(75, 23);
+            this.enviar4.TabIndex = 12;
+            this.enviar4.Text = "ENVIAR";
+            this.enviar4.UseVisualStyleBackColor = true;
+            this.enviar4.Click += new System.EventHandler(this.enviar4_Click);
             // 
             // httpSend
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(949, 268);
+            this.Controls.Add(this.enviar4);
+            this.Controls.Add(this.enviar3);
+            this.Controls.Add(this.salvar4);
+            this.Controls.Add(this.salvar3);
+            this.Controls.Add(this.endpoint4);
+            this.Controls.Add(this.endpoint3);
+            this.Controls.Add(this.enviar2);
+            this.Controls.Add(this.salvar2);
+            this.Controls.Add(this.endpoint2);
             this.Controls.Add(this.respostaHttpBox);
             this.Controls.Add(this.enviar1);
             this.Controls.Add(this.salvar1);
@@ -91,6 +192,15 @@
         private System.Windows.Forms.Button salvar1;
         private System.Windows.Forms.Button enviar1;
         private System.Windows.Forms.TextBox respostaHttpBox;
+        private System.Windows.Forms.TextBox endpoint2;
+        private System.Windows.Forms.Button salvar2;
+        private System.Windows.Forms.Button enviar2;
+        private System.Windows.Forms.TextBox endpoint3;
+        private System.Windows.Forms.TextBox endpoint4;
+        private System.Windows.Forms.Button salvar3;
+        private System.Windows.Forms.Button salvar4;
+        private System.Windows.Forms.Button enviar3;
+        private System.Windows.Forms.Button enviar4;
     }
 }
 
--- httpSend/httpSend.cs
+++ httpSend/httpSend.cs
@@ -44,13 +44,117 @@ namespace httpSend
 
         private void salvar1_Click(object sender, EventArgs e)
         {
-            string url = endpoint1.Text;
-            this.endpoint1.ReadOnly = true;
+            if (this.endpoint1.ReadOnly == false)
+            {
+                if (endpoint1.Text != "")
+                {
+                    string url = endpoint1.Text;
+                    this.endpoint1.ReadOnly = true;
+                }
+                else
+                {
+                    MessageBox.Show("O campo está vazio!");
+                }
+
+            }
+            else
+            {
+                this.endpoint1.ReadOnly = false;
+                endpoint1.Text = "";
+            }
         }
 
         private async void enviar1_Click(object sender, EventArgs e)
         {
             string url = endpoint1.Text;
+            await reqHttp(url);
+            respostaHttpBox.Text = respostaHttp;
+        }
+
+        private void salvar2_Click(object sender, EventArgs e)
+        {
+            if (this.endpoint2.ReadOnly == false)
+            {
+                if(endpoint2.Text != "")
+                {
+                    string url = endpoint2.Text;
+                    this.endpoint2.ReadOnly = true;
+                }
+                else
+                {
+                    MessageBox.Show("O campo está vazio!");
+                }
+                
+            }
+            else
+            {
+                this.endpoint2.ReadOnly = false;
+                endpoint2.Text = "";
+            }
+            
+        }
+
+        private async void enviar2_Click(object sender, EventArgs e)
+        {
+            string url = endpoint2.Text;
+            await reqHttp(url);
+            respostaHttpBox.Text = respostaHttp;
+        }
+
+        private void salvar3_Click(object sender, EventArgs e)
+        {
+            if (this.endpoint3.ReadOnly == false)
+            {
+                if (endpoint3.Text != "")
+                {
+                    string url = endpoint3.Text;
+                    this.endpoint3.ReadOnly = true;
+                }
+                else
+                {
+                    MessageBox.Show("O campo está vazio!");
+                }
+
+            }
+            else
+            {
+                this.endpoint3.ReadOnly = false;
+                endpoint3.Text = "";
+            }
+        }
+
+        private async void enviar3_Click(object sender, EventArgs e)
+        {
+            string url = endpoint3.Text;
+            await reqHttp(url);
+            respostaHttpBox.Text = respostaHttp;
+        }
+
+        private void salvar4_Click(object sender, EventArgs e)
+        {
+            if (this.endpoint4.ReadOnly == false)
+            {
+                if (endpoint4.Text != "")
+                {
+                    string url = endpoint4.Text;
+                    this.endpoint4.ReadOnly = true;
+                }
+                else
+                {
+                    MessageBox.Show("O campo está vazio!");
+                }
+
+            }
+            else
+            {
+                this.endpoint4.ReadOnly = false;
+                endpoint4.Text = "";
+            }
+        }
+
+        private async void enviar4_Click(object sender, EventArgs e)
+        {
+            string url = endpoint4.Text;
             await reqHttp(url);
             respostaHttpBox.Text = respostaHttp;
         }
